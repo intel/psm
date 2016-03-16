intel/psm

commit 2b70a0f379c1a72b924bc4d5cf6ea19749c0bd57

From f07845ca7692b02c4bc9aef7fda3ee502c339085 Mon Sep 17 00:00:00 2001 Subject: [PATCH 1/3] Add Support for more than 64K ranks,
commidx size increased from 16 to 20 bits to allow for 2^20 connection ids.
The base connection id was changed from a pseudo random number to 0.
This allows backwards compatibility with other PSM libraries, provided that
the job uses less than 64K ranks.

include/ipath_common.h
@@ -673,6 +673,12 @@ struct ipath_flash {
 /* K_PktFlags bits */
 #define INFINIPATH_KPF_INTR 0x1
 #define INFINIPATH_KPF_HDRSUPP 0x2
+#define INFINIPATH_KPF_INTR_HDRSUPP_MASK 0x3
+#define INFINIPATH_KPF_COMMIDX_MASK 0xF
+#define INFINIPATH_KPF_COMMIDX_SHIFT 2
+#define INFINIPATH_KPF_RESERVED_BITS(pkt_flags)            \
+  ((__le16_to_cpu(pkt_flags) << IPS_EPSTATE_COMMIDX_SHIFT) \
+  & INFINIPATH_KPF_COMMIDX_MASK)                           \
 
 #define INFINIPATH_MAX_SUBCONTEXT	4
 

ptl_ips/ips_epstate.c
@@ -49,7 +49,7 @@ ips_epstate_init(struct ips_epstate *eps, const psmi_context_t *context)
 {
     memset(eps, 0, sizeof(*eps));
     eps->context = context;
-    eps->eps_base_idx = (ips_epstate_idx) get_cycles();
+    eps->eps_base_idx = 0;
     return PSM_OK;
 }
 
@@ -102,8 +102,7 @@ ips_epstate_add(struct ips_epstate *eps, struct ptl_epaddr *ipsaddr,
 	}
     }
     psmi_assert_always(i != eps->eps_tabsize);
-    commidx = j;
-    commidx -= eps->eps_base_idx;
+    commidx = j & (IPS_EPSTATE_COMMIDX_MAX - 1);
     _IPATH_VDBG("node %s gets commidx=%d (table idx %d)\n", 
 	    psmi_epaddr_get_name(ipsaddr->epaddr->epid), commidx, j);
     eps->eps_tab[j].epid = 
@@ -123,8 +122,9 @@ ips_epstate_add(struct ips_epstate *eps, struct ptl_epaddr *ipsaddr,
 psm_error_t
 ips_epstate_del(struct ips_epstate *eps, ips_epstate_idx commidx)
 {
-    ips_epstate_idx idx = commidx;
-    idx += eps->eps_base_idx; /* actual table index */
+    ips_epstate_idx idx;
+    /* actual table index */
+    idx = commidx & (IPS_EPSTATE_COMMIDX_MAX - 1);
     psmi_assert_always(idx < eps->eps_tabsize);
     _IPATH_VDBG("commidx=%d, table_idx=%d\n", commidx, idx);
     eps->eps_tab[idx].epid = 0;

ptl_ips/ips_epstate.h
@@ -36,8 +36,10 @@
 
 #include "psm_user.h"
 
-typedef uint16_t ips_epstate_idx;
-#define IPS_EPSTATE_COMMIDX_MAX 65535
+typedef uint32_t ips_epstate_idx;
+#define IPS_EPSTATE_COMMIDX_MAX (1<<20)
+#define IPS_EPSTATE_COMMIDX_MASK 0x3C
+#define IPS_EPSTATE_COMMIDX_SHIFT 14
 
 struct ptl_epaddr;
 
@@ -68,7 +70,7 @@ PSMI_INLINE(
 struct ips_epstate_entry *
 ips_epstate_lookup(const struct ips_epstate *eps, ips_epstate_idx idx))
 {
-    idx += eps->eps_base_idx;
+    idx = idx & (IPS_EPSTATE_COMMIDX_MAX - 1);
     if (idx < eps->eps_tabsize)
 	return &eps->eps_tab[idx];
     else

ptl_ips/ips_proto.c
@@ -1020,7 +1020,10 @@ _build_ctrl_message(struct ips_proto *proto,
         (IPS_PROTO_VERSION << INFINIPATH_I_VERS_SHIFT) +
         (epr->epr_pkt_context << INFINIPATH_I_CONTEXT_SHIFT) +
         (IPATH_EAGER_TID_ID << INFINIPATH_I_TID_SHIFT));
-    p_hdr->iph.pkt_flags = __cpu_to_le16(pkt_flags);
+    p_hdr->iph.pkt_flags = __cpu_to_le16(
+        (pkt_flags & INFINIPATH_KPF_INTR_HDRSUPP_MASK) |
+        ((epr->epr_commidx_to >> IPS_EPSTATE_COMMIDX_SHIFT)
+        & IPS_EPSTATE_COMMIDX_MASK));
     
     ips_kdeth_cksum(p_hdr);  // Generate KDETH  checksum
     

ptl_ips/ips_proto_help.h
@@ -40,6 +40,7 @@
 #include "ipserror.h"
 #include "psm_mq_internal.h" // psmi_mq_handle_tiny_envelope
 #include "ptl_ips.h"
+#include "ips_epstate.h"
 
 /* Some tunable compile-time options */
 #define IPS_TINY_PROCESS_MQTINY 1   /* whether mq processing of tiny pkts is
@@ -315,7 +316,8 @@ void ips_proto_hdr(ips_scb_t *scb,
 		(scb->offset >> 2)); // convert from byte to word offset
 
 	p_hdr->lrh[2] = __cpu_to_be16(paywords + SIZE_OF_CRC);
-	p_hdr->iph.pkt_flags = __cpu_to_le16(kpf_flags);
+	p_hdr->iph.pkt_flags |= __cpu_to_le16(
+    (kpf_flags & INFINIPATH_KPF_INTR_HDRSUPP_MASK));
 
 	ips_kdeth_cksum(p_hdr); // Generate KDETH checksum
 
@@ -348,7 +350,9 @@ void ips_proto_hdr(ips_scb_t *scb,
         (epr->epr_pkt_context << INFINIPATH_I_CONTEXT_SHIFT) +
         (scb->tid << INFINIPATH_I_TID_SHIFT) +
         (scb->offset >> 2)); // convert from byte to word offset
-    p_hdr->iph.pkt_flags = __cpu_to_le16(kpf_flags);
+    p_hdr->iph.pkt_flags = __cpu_to_le16(
+      kpf_flags | ((epr->epr_commidx_to >> IPS_EPSTATE_COMMIDX_SHIFT) &
+      IPS_EPSTATE_COMMIDX_MASK));
     
     ips_kdeth_cksum(p_hdr); // Generate KDETH checksum
 

ptl_ips/ips_proto_dump.c
@@ -36,6 +36,7 @@
 #include "ips_proto_internal.h"
 #include "ips_proto_header.h"
 #include "ips_proto_help.h"
+#include "ips_epstate.h"
 
 void ips_proto_dump_frame(void *frame, int lenght, char *message)
 {
@@ -99,7 +100,8 @@ void ips_proto_show_header(struct ips_message_header *p_hdr, char *msg)
     printf("BTH: R8-DestQP24 %x\n", __be32_to_cpu(p_hdr->bth[1]));
     printf("BTH: AR1-Res7-PSN24 %x\n", __be32_to_cpu(p_hdr->bth[2]));
     printf("IPH: chksum %x\n", __le16_to_cpu(p_hdr->iph.chksum));
-    printf("IPH: pkt_flags %x\n", __le16_to_cpu(p_hdr->iph.pkt_flags));
+    printf("IPH: pkt_flags %x\n", __le16_to_cpu(
+        p_hdr->iph.pkt_flags) & INFINIPATH_KPF_INTR_HDRSUPP_MASK);
     printf("IPH: ver %i\n",
         (__le32_to_cpu(p_hdr->iph.ver_context_tid_offset)
         >> INFINIPATH_I_VERS_SHIFT) & INFINIPATH_I_VERS_MASK);
@@ -126,7 +128,8 @@ void ips_proto_show_header(struct ips_message_header *p_hdr, char *msg)
     printf("context %d (src_context %d src_context_ext %d) src_subcontext %d\n",
 	IPS_HEADER_SRCCONTEXT_GET(p_hdr), p_hdr->src_context, p_hdr->src_context_ext,
 	p_hdr->src_subcontext);
-    printf("src_rank/commidx %i\n", p_hdr->commidx);
+    printf("src_rank/commidx %i\n", p_hdr->commidx |
+        INFINIPATH_KPF_RESERVED_BITS(p_hdr->iph.pkt_flags));
     if (tid != IPATH_EAGER_TID_ID)
 	printf("expected_tid_session_id %i\n", p_hdr->data[0].u32w0);
     printf("flags %x\n", p_hdr->flags);

ptl_ips/ips_proto_recv.c
@@ -1263,7 +1263,8 @@ ips_proto_process_packet_error(struct ips_recvhdrq_event *rcv_ev)
 	    
 	    /* Obtain ipsaddr for packet */
 	    epstaddr = ips_epstate_lookup(rcv_ev->recvq->epstate, 
-					  rcv_ev->p_hdr->commidx);	    
+        rcv_ev->p_hdr->commidx +
+        INFINIPATH_KPF_RESERVED_BITS(p_hdr->iph.pkt_flags));
 	    if_pf (epstaddr == NULL || epstaddr->epid != rcv_ev->epid)
 	      return 0; /* Unknown packet - drop */
 	    

ptl_ips/ips_recvhdrq.c
@@ -367,7 +367,8 @@ do_pkt_cksum(struct ips_recvhdrq_event *rcv_ev)
     uint32_t hd, tl;
     
     epstaddr =
-      ips_epstate_lookup(rcv_ev->recvq->epstate, rcv_ev->p_hdr->commidx);
+      ips_epstate_lookup(rcv_ev->recvq->epstate, rcv_ev->p_hdr->commidx +
+          INFINIPATH_KPF_RESERVED_BITS(rcv_ev->p_hdr->iph.pkt_flags));
     epstaddr = (epstaddr && epstaddr->ipsaddr) ? epstaddr : NULL;
     
     lcontext = 
@@ -580,7 +581,9 @@ ips_recvhdrq_progress(struct ips_recvhdrq *recvq)
 	    /* Classify packet from a known or unknown endpoint */
 	    struct ips_epstate_entry *epstaddr;
 
-	    epstaddr = ips_epstate_lookup(recvq->epstate, rcv_ev.p_hdr->commidx);	    
+      epstaddr =
+        ips_epstate_lookup(recvq->epstate, rcv_ev.p_hdr->commidx +
+            INFINIPATH_KPF_RESERVED_BITS(rcv_ev.p_hdr->iph.pkt_flags));
 	    if_pf (epstaddr == NULL || epstaddr->epid != rcv_ev.epid) {
 	        rcv_ev.ipsaddr = NULL;
 		recvq->recvq_callbacks.callback_packet_unknown(&rcv_ev);

commit 67c0807c74e9d445900d5541358f0f575f22a630

Fix hang with 64K rank support when using Intel MPI. During MPI_Finalize with Intel MPI, many processes would hang in ips_proto_mq_isend. This problem was only noticed at a larger amount of ranks and nodes. This bug was introduced in 2b70a0f379c1a72b924bc4d5cf6ea19749c0bd57. There were some bit shift and mask errors with the change. The commidx base id was not properly used correctly in ips_epstate functions, causing some messages to never get received properly.

include/ipath_common.h
@@ -674,11 +674,11 @@ struct ipath_flash {
 #define INFINIPATH_KPF_INTR 0x1
 #define INFINIPATH_KPF_HDRSUPP 0x2
 #define INFINIPATH_KPF_INTR_HDRSUPP_MASK 0x3
-#define INFINIPATH_KPF_COMMIDX_MASK 0xF
+#define INFINIPATH_KPF_COMMIDX_MASK 0x003C
 #define INFINIPATH_KPF_COMMIDX_SHIFT 2
-#define INFINIPATH_KPF_RESERVED_BITS(pkt_flags)            \
-  ((__le16_to_cpu(pkt_flags) << IPS_EPSTATE_COMMIDX_SHIFT) \
-  & INFINIPATH_KPF_COMMIDX_MASK)                           \
+#define INFINIPATH_KPF_RESERVED_BITS(pktflags)            \
+  ((__le16_to_cpu(pktflags) & INFINIPATH_KPF_COMMIDX_MASK) \
+    << IPS_EPSTATE_COMMIDX_SHIFT) \
 
 #define INFINIPATH_MAX_SUBCONTEXT	4
 

ptl_ips/ips_epstate.c
@@ -49,7 +49,8 @@ ips_epstate_init(struct ips_epstate *eps, const psmi_context_t *context)
 {
     memset(eps, 0, sizeof(*eps));
     eps->context = context;
-    eps->eps_base_idx = 0;
+    eps->eps_base_idx = ((ips_epstate_idx)get_cycles()) &
+      (IPS_EPSTATE_COMMIDX_MAX-1);
     return PSM_OK;
 }
 
@@ -102,7 +103,7 @@ ips_epstate_add(struct ips_epstate *eps, struct ptl_epaddr *ipsaddr,
 	}
     }
     psmi_assert_always(i != eps->eps_tabsize);
-    commidx = j & (IPS_EPSTATE_COMMIDX_MAX - 1);
+    commidx = (j - eps->eps_base_idx) & (IPS_EPSTATE_COMMIDX_MAX-1);
     _IPATH_VDBG("node %s gets commidx=%d (table idx %d)\n", 
 	    psmi_epaddr_get_name(ipsaddr->epaddr->epid), commidx, j);
     eps->eps_tab[j].epid = 
@@ -124,7 +125,7 @@ ips_epstate_del(struct ips_epstate *eps, ips_epstate_idx commidx)
 {
     ips_epstate_idx idx;
     /* actual table index */
-    idx = commidx & (IPS_EPSTATE_COMMIDX_MAX - 1);
+    idx = (commidx + eps->eps_base_idx) & (IPS_EPSTATE_COMMIDX_MAX-1);
     psmi_assert_always(idx < eps->eps_tabsize);
     _IPATH_VDBG("commidx=%d, table_idx=%d\n", commidx, idx);
     eps->eps_tab[idx].epid = 0;

ptl_ips/ips_epstate.h
@@ -38,8 +38,11 @@
 
 typedef uint32_t ips_epstate_idx;
 #define IPS_EPSTATE_COMMIDX_MAX (1<<20)
-#define IPS_EPSTATE_COMMIDX_MASK 0x3C
+#define IPS_EPSTATE_COMMIDX_MASK 0xF0000
 #define IPS_EPSTATE_COMMIDX_SHIFT 14
+#define IPS_EPSTATE_COMMIDX_PACK(ipscommidx) \
+  ((ipscommidx & IPS_EPSTATE_COMMIDX_MASK) \
+    >> IPS_EPSTATE_COMMIDX_SHIFT)
 
 struct ptl_epaddr;
 
@@ -70,7 +73,7 @@ PSMI_INLINE(
 struct ips_epstate_entry *
 ips_epstate_lookup(const struct ips_epstate *eps, ips_epstate_idx idx))
 {
-    idx = idx & (IPS_EPSTATE_COMMIDX_MAX - 1);
+  idx = (idx + eps->eps_base_idx) & (IPS_EPSTATE_COMMIDX_MAX-1);
     if (idx < eps->eps_tabsize)
 	return &eps->eps_tab[idx];
     else

ptl_ips/ips_proto.c
@@ -762,7 +762,7 @@ _build_ctrl_message(struct ips_proto *proto,
     uint32_t tot_paywords = sizeof(struct ips_message_header) >> 2;
     struct ips_epinfo *epinfo = &proto->epinfo;
     struct ips_epinfo_remote *epr = &ipsaddr->epr;
-    uint16_t pkt_flags = 0;
+    uint16_t pkt_flags = IPS_EPSTATE_COMMIDX_PACK(epr->epr_commidx_to);
     struct ips_message_header *p_hdr = &msg->pbc_hdr.hdr;
     ips_path_rec_t *ctrl_path = ipsaddr->epr.epr_path[IPS_PATH_HIGH_PRIORITY][ipsaddr->epr.epr_hpp_index];
     int paylen = 0;
@@ -1021,10 +1021,7 @@ _build_ctrl_message(struct ips_proto *proto,
         (IPS_PROTO_VERSION << INFINIPATH_I_VERS_SHIFT) +
         (epr->epr_pkt_context << INFINIPATH_I_CONTEXT_SHIFT) +
         (IPATH_EAGER_TID_ID << INFINIPATH_I_TID_SHIFT));
-    p_hdr->iph.pkt_flags = __cpu_to_le16(
-        (pkt_flags & INFINIPATH_KPF_INTR_HDRSUPP_MASK) |
-        ((epr->epr_commidx_to >> IPS_EPSTATE_COMMIDX_SHIFT)
-        & IPS_EPSTATE_COMMIDX_MASK));
+    p_hdr->iph.pkt_flags = __cpu_to_le16(pkt_flags);
     
     ips_kdeth_cksum(p_hdr);  // Generate KDETH  checksum
     

ptl_ips/ips_proto_help.h
@@ -316,8 +316,7 @@ void ips_proto_hdr(ips_scb_t *scb,
 		(scb->offset >> 2)); // convert from byte to word offset
 
 	p_hdr->lrh[2] = __cpu_to_be16(paywords + SIZE_OF_CRC);
-	p_hdr->iph.pkt_flags |= __cpu_to_le16(
-    (kpf_flags & INFINIPATH_KPF_INTR_HDRSUPP_MASK));
+	p_hdr->iph.pkt_flags = __cpu_to_le16(kpf_flags);
 
 	ips_kdeth_cksum(p_hdr); // Generate KDETH checksum
 
@@ -350,9 +349,7 @@ void ips_proto_hdr(ips_scb_t *scb,
         (epr->epr_pkt_context << INFINIPATH_I_CONTEXT_SHIFT) +
         (scb->tid << INFINIPATH_I_TID_SHIFT) +
         (scb->offset >> 2)); // convert from byte to word offset
-    p_hdr->iph.pkt_flags = __cpu_to_le16(
-      kpf_flags | ((epr->epr_commidx_to >> IPS_EPSTATE_COMMIDX_SHIFT) &
-      IPS_EPSTATE_COMMIDX_MASK));
+    p_hdr->iph.pkt_flags = __cpu_to_le16(kpf_flags);
     
     ips_kdeth_cksum(p_hdr); // Generate KDETH checksum
 
@@ -387,7 +384,7 @@ ips_scb_prepare_flow_inner(ips_scb_t *scb,
 {
     uint32_t extra_bytes;
     uint32_t tot_paywords;
-    uint16_t pkt_flags = 0;
+    uint16_t pkt_flags = IPS_EPSTATE_COMMIDX_PACK(epr->epr_commidx_to);
     
     extra_bytes = scb->payload_size & 3;
     if (extra_bytes) {